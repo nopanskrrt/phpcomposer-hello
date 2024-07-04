d474eefee5819d7de70f4e3fd6238dafee46cc1c
Novan Febrian <novan.novan8899@gmail.com>
2024-07-04 13:47:23 +0700

    Add default value

---
+++ src/Customer.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace NovanFebrian\Belajar;
+
+class Customer {
+    public function __construct(private string $name)
+    {
+        
+    }
+
+    public function sayHello(string $name = "Guest"):string {
+        return "Hello $name, My Name is $this->name";
+    }
+}
